4c5041ba0e1a63e45934f8df12d56a61f8c2404c
pnpjss <pnpjss@github.com>
2021-04-13 09:21:18 +0100

    test

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -1,12 +1,21 @@
 @extends('/layouts/app')
 
 
-{{$quizid}}
+<h1>
+
+    Result
+    {{$quizid}}
+</h1>
+<br>
+
 {{$request->answer1}}
 {{$request->answer2}}
 {{$request->answer3}}
 {{$request->answer4}}
 {{$request->answer5}}
 
+
+<a href="/dashboard">dashboard</a>
+<a href="/history">history</a>
 
 {{-- styleci --}}
